782c2ecf4ca6eb6219f63f17119a7b8a14fea270
Gabriel Monge <gabriel.monge.lizano@gmail.com>
2022-07-13 16:44:57 -0600

    major changes on crud functioning

--- server/index.js
+++ server/index.js
@@ -6,39 +6,84 @@ const libPath = "C:\\oracle\\instantclient_21_6";
 const fs = require("fs");
 const PORT = process.env.PORT || 4000;
 const app = express();
+const db = require("./db/dbuse.js");
 
-if (libPath && fs.existsSync(libPath)) {
-  oracledb.initOracleClient({ libDir: libPath });
-}
 
-async function run() {
-  let connection;
-  try {
-    connection = await oracledb.getConnection(dbconfig);
-    //await setupBf(connection);
-    const result = await connection.execute(`SELECT * FROM BF`);
-    console.log(result.rows);
-    return result.rows;
+app.get("/create", async (req, res) => {
+  db.insertToDB(3, "gabriel", 22);
+  res.send("ok");
+});
 
-  } catch (err) {
-    console.error(err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-}
+app.get ("/read", async (req, res) => {
+  db.readFromDB(6);
+  res.send("ok");
+});
+
+app.get ("/readAll", async (req, res) => {
+  db.readAllfromDB();
+  res.send("ok");
+});
+
+app.get("/update", async (req, res) => {
+  db.updateDB(6, "gabriel", 20);
+  res.send("ok");
+});
+
+app.get("/delete", async (req, res) => {
+  db.deleteFromDB(3);
+  res.send("ok");
+});
 
 app.get("/api", (req, res) => {
-  run().then(result => {
-    res.send(result);
-  });
+
+  db.run().then((data) => {
+    res.send(data);
+  } ).catch((err) => {
+    console.error(err);
+  }
+  );
+  // run().then(result => {
+  //   console.log(result);
+  //   res.send(result);
+  // });
 });
   // estoy mamando aquí
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
+async function setupBf(connection) {
+
+  try {
+
+    const stmts = [
+
+      `ALTER SESSION SET nls_date_format = 'YYYY-MM-DD HH24:MI:SS'`,
+
+      `CREATE TABLE PRUEBA (
+         id       NUMBER NOT NULL,
+         name   VARCHAR2(40),
+         age   NUMBER
+       )`,
+
+      `INSERT INTO PRUEBA VALUES (1, 'Juan', 20)`,
+      `INSERT INTO PRUEBA VALUES (2, 'Pedro', 30)`,
+      `INSERT INTO PRUEBA VALUES (3, 'Maria', 40)`,
+
+    ];
+
+    for (const s of stmts) {
+      try {
+        await connection.execute(s);
+      } catch (e) {
+        if (e.errorNum != 942)
+          throw (e);
+      }
+    }
+    await connection.commit();
+
+  } catch (err) {
+    console.error(err);
+  }
+}
